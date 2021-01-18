291127ac1d3cc061d86eb988c6e4ab0755d3bf1f
Sergey Tkachev <sergey25121995@yandex.ru>
2021-01-18 21:35:29 +0300

    Initial commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public Transform[] SpawnPlaces;
+        public GameObject Enemy;
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                foreach (var SpawnPlace in SpawnPlaces)
+                {
+                    Instantiate(Enemy, SpawnPlace.position, transform.rotation);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 2f;
+        [SerializeField] private Vector3 _direction = Vector3.zero;
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            _direction.x = Input.GetAxis("Horizontal");
+            _direction.z = Input.GetAxis("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            var speed = _direction * _speed * Time.fixedDeltaTime;
+            transform.Translate(speed);
+        }
+    }
+}
