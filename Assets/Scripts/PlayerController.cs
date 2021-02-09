6509a18c16d1ada79bc922a3d1e534af7b93af5c
Sergey Tkachev <sergey25121995@yandex.ru>
2021-02-09 18:59:21 +0300

    added player jump and traps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,21 @@ namespace Assets.Scripts
 {
     public class PlayerController : MonoBehaviour, ITakeDamage
     {
+        [SerializeField] private Transform _jumpTransform;
+        [SerializeField] private LayerMask _layerMask;
+
         [SerializeField] private float _moveSpeed = 4f;
+        [SerializeField] private float _jumpSpeed = 5f;
         [SerializeField] private float _rotateSpeed = 6f;
+        [SerializeField] private float _rayCastLength = 0.1f;
 
         [SerializeField] private int _currentHealth = 10;
         [SerializeField] private int _maxHealth = 10;
 
         private Rigidbody _rigidbody;
         private Vector3 _direction = Vector3.zero;
+
+        private bool _jump;
 
         public void TakeDamage(int damage)
         {
@@ -46,10 +53,22 @@ namespace Assets.Scripts
         {
             _direction.x = Input.GetAxisRaw("Horizontal");
             _direction.z = Input.GetAxisRaw("Vertical");
+
+            _jump = Input.GetKeyDown(KeyCode.Space);
         }
 
         private void FixedUpdate()
         {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (_jump)
+            {
+                Jump();
+            }
+
             if (_direction != Vector3.zero)
             {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_direction),
@@ -57,6 +76,16 @@ namespace Assets.Scripts
             }
 
             _rigidbody.MovePosition(transform.position + _moveSpeed * Time.fixedDeltaTime * _direction);
+        }
+
+        private void Jump()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_jumpTransform.position, _jumpTransform.TransformDirection(Vector3.forward), out hit,
+                _rayCastLength, _layerMask))
+            {
+                _rigidbody.AddForce(transform.up * _jumpSpeed, ForceMode.Impulse);
+            }
         }
     }
 }
---
+++ Assets/Scripts/TrapCutterController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class TrapCutterController : MonoBehaviour
+    {
+        [SerializeField] private int _damage = 2;
+
+        private Animation _anim;
+
+        private void Awake()
+        {
+            _anim = gameObject.GetComponentInParent<Animation>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (_anim.isPlaying)
+                {
+                    var player = other.gameObject.GetComponent<PlayerController>();
+                    player.TakeDamage(_damage);
+                }
+            }
+
+            if (other.gameObject.CompareTag("Rock"))
+            {
+                _anim.Stop();
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject.CompareTag("Rock"))
+            {
+                _anim.Play();
+            }
+        }
+    }
+}
